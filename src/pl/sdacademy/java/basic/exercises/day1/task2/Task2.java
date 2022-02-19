fd69ca0462702dd10cbc46c3b95fe275b51e8679
DamianZygma <damian.zygma@gmail.com>
2022-02-19 11:31:17 +0100

    task2-poprawiony

--- src/pl/sdacademy/java/basic/exercises/day1/task2/Task2.java
+++ src/pl/sdacademy/java/basic/exercises/day1/task2/Task2.java
@@ -3,41 +3,33 @@ package pl.sdacademy.java.basic.exercises.day1.task2;
 import java.util.Scanner;
 
 public class Task2 {
-//    private static final float PI = 3.14f;
-
+    private static final float PI = 3.14f;
 
     public static void main(String[] args) {
-//        area = pi * r * r
-//        perimeter = 2 * pi * r
         float diameter = getDiameterFromUser();
 
         float circumference = calculateCircumferenceOfACircle(diameter);
-        System.out.println("Circumference Of A Circle: " = circumference);
+        System.out.println("Circumference Of A Circle: " + circumference);
 
         float area = calculateAreaOfACircle(diameter);
         System.out.println("Area Of A Circle: " + area);
-
-
     }
 
     private static float getDiameterFromUser() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please insert diameter:");
+        System.out.print("Please insert diameter: ");
         return scanner.nextFloat();
     }
 
     private static float calculateCircumferenceOfACircle(float diameter) {
-//        return PI * diameter;
+        // 2 * pi * r -> pi * d
+        //return PI * diameter;
         return (float) Math.PI * diameter;
     }
 
-    private static float calculateAreaOfACircle (float diameter) {
-
-        float radius = diameter/2;
+    private static float calculateAreaOfACircle(float diameter) {
+        // pi * r ^ 2
+        float radius = diameter / 2;
         return (float) Math.PI * radius * radius;
     }
-
-
-
-
 }
