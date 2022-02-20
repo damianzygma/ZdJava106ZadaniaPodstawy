622cb156d603bb95e1d9ec36898bff6422b24cc6
DamianZygma <damian.zygma@gmail.com>
2022-02-20 14:26:06 +0100

    day2_task11

---
+++ src/pl/sdacademy/java/basic/exercises/day2/task11/Task11.java
@@ -0,0 +1,25 @@
+package pl.sdacademy.java.basic.exercises.day2.task11;
+
+public class Task11 {
+    public static void main(String[] args) {
+        String input = "kajak";
+        System.out.println("Input: " + input);
+        System.out.println("Result: " + ifPalindrome(input));
+
+        System.out.println();
+
+        input = "kajak";
+        System.out.println("Input: " + input);
+        System.out.println("Result: " + ifPalindrome(input));
+
+    }
+
+    private static boolean ifPalindrome(String input) {
+        if(input != null) {
+            String AfterReverse = new StringBuilder(input).reverse().toString();
+            return input.equals(AfterReverse);
+        }
+        return false;
+    }
+
+}
